d42b7098e001cc42e0c0a7d2d580e27143bce133
Nakhoon <cnh6123@naver.com>
2025-07-31 20:17:07 +0900

    [INIT] 기본 데이터 생성용 data.sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO meetingroom.meeting_room (active, capacity, hourly_fee, id, name) VALUES (true, 8, 8000.00, 1, 'meeting-room-A');
+INSERT INTO meetingroom.meeting_room (active, capacity, hourly_fee, id, name) VALUES (true, 12, 12000.00, 2, 'meeting-room-B');
+INSERT INTO meetingroom.meeting_room (active, capacity, hourly_fee, id, name) VALUES (true, 16, 16000.00, 3, 'meeting-room-C');
+INSERT INTO meetingroom.meeting_room (active, capacity, hourly_fee, id, name) VALUES (true, 10, 10000.00, 4, 'meeting-room-D');
+INSERT INTO meetingroom.meeting_room (active, capacity, hourly_fee, id, name) VALUES (false, 12, 6000.00, 5, 'meeting-room-E');
+
+INSERT INTO meetingroom.user (id, email, name) VALUES (1, 'A@gmail.com', 'A');
+INSERT INTO meetingroom.user (id, email, name) VALUES (2, 'B@daum.net', 'B');
+INSERT INTO meetingroom.user (id, email, name) VALUES (3, 'C@naver.com', 'C');
+
