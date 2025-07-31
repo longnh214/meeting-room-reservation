f2a1fe810ed5565ce4f6df077f85a7771abe3b9a
Nakhoon <cnh6123@naver.com>
2025-07-31 13:48:32 +0900

    [UPDATE] PaymentStatus 상태 요구 사항에 맞춰서 변경

--- src/main/java/com/example/reservation/type/PaymentStatus.java
+++ src/main/java/com/example/reservation/type/PaymentStatus.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public enum PaymentStatus{
 
     PENDING("P", "결제 대기"),
-    PAID("S", "결제 완료"),
+    SUCCESS("S", "결제 완료"),
     FAILED("F", "결제 실패"),
     CANCELLED("C", "결제 취소"),
     REFUNDED("R", "환불 완료");
