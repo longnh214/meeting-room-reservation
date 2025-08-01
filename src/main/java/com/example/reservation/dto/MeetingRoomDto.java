e029fc48f73aa596371f91281be56af17b912657
Nakhoon <cnh6123@naver.com>
2025-08-01 10:55:24 +0900

    [UPDATE] meeting-rooms dto optimize imports

--- src/main/java/com/example/reservation/dto/MeetingRoomDto.java
+++ src/main/java/com/example/reservation/dto/MeetingRoomDto.java
@@ -1,7 +1,10 @@
 package com.example.reservation.dto;
 
 import com.example.reservation.entity.MeetingRoom;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
---
+++ src/main/java/com/example/reservation/validator/HalfHourValidator.java
@@ -0,0 +1,4 @@
+package com.example.reservation.validator;
+
+public class HalfHourValidator {
+}
