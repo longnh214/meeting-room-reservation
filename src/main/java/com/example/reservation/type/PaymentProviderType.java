f8906dd1bfab9fdc87dfc20aedf5c7e99804e062
Nakhoon <cnh6123@naver.com>
2025-08-03 15:47:21 +0900

    [UPDATE] payment 결제사 타입 내 결제 수단 타입 추가

--- src/main/java/com/example/reservation/type/PaymentProviderType.java
+++ src/main/java/com/example/reservation/type/PaymentProviderType.java
@@ -6,22 +6,24 @@ import java.util.Arrays;
 
 @Getter
 public enum PaymentProviderType {
-    A("A", "A 회사"),
-    B("B", "B 회사"),
-    C("C", "C 회사");
+    A("A", "A 회사", PaymentMeasurementType.CARD),
+    B("B", "B 회사", PaymentMeasurementType.SIMPLE),
+    C("C", "C 회사", PaymentMeasurementType.VIRTUAL_ACCOUNT);
 
     private final String code;
     private final String description;
+    private final PaymentMeasurementType paymentMeasurementType;
 
-    PaymentProviderType(String code, String description) {
+    PaymentProviderType(String code, String description, PaymentMeasurementType paymentMeasurementType) {
         this.code = code;
         this.description = description;
+        this.paymentMeasurementType = paymentMeasurementType;
     }
 
     public static PaymentProviderType fromCode(String code) {
         return Arrays.stream(PaymentProviderType.values())
                 .filter(s -> s.code.equals(code))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("알 수 없는 결제 제공사 코드입니다. : " + code));
+                .orElseThrow(() -> new IllegalArgumentException("알 수 없는 결제사 코드입니다. : " + code));
     }
 }
