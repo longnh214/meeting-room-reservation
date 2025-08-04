47307f61c4ca39c1e1b64730ab3ad2cac191def3
Nakhoon <cnh6123@naver.com>
2025-08-04 11:33:58 +0900

    [UPDATE] 결제 관련 통합 테스트 작성 완료

--- src/test/java/com/example/reservation/service/impl/PaymentIntegrationTest.java
+++ src/test/java/com/example/reservation/service/impl/PaymentIntegrationTest.java
@@ -2,34 +2,21 @@ package com.example.reservation.service.impl;
 
 import com.example.reservation.AbstractIntegrationTest;
 import com.example.reservation.dto.PaymentDto;
-import com.example.reservation.entity.MeetingRoom;
-import com.example.reservation.entity.Payment;
-import com.example.reservation.entity.PaymentProvider;
-import com.example.reservation.entity.Reservation;
-import com.example.reservation.entity.User;
+import com.example.reservation.entity.*;
 import com.example.reservation.repository.*;
-import com.example.reservation.service.PaymentProviderExternalService;
 import com.example.reservation.service.PaymentService;
 import com.example.reservation.type.PaymentProviderType;
 import com.example.reservation.type.PaymentStatus;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.testcontainers.shaded.org.awaitility.Awaitility;
 
 import java.math.BigDecimal;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
 
 class PaymentIntegrationTest extends AbstractIntegrationTest {
 
@@ -62,6 +49,8 @@ class PaymentIntegrationTest extends AbstractIntegrationTest {
 
     @BeforeEach
     void setup() {
+        paymentRepository.deleteAll();
+        paymentProviderRepository.deleteAll();
         reservationRepository.deleteAll();
         meetingRoomRepository.deleteAll();
         userRepository.deleteAll();
@@ -111,8 +100,10 @@ class PaymentIntegrationTest extends AbstractIntegrationTest {
     }
 
     @Test
-    void 결제_성공_프로세스_MOCK_테스트() throws InterruptedException {
+    void 결제_성공_프로세스_통합_테스트() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
+
+        PaymentStatus 성공 = PaymentStatus.SUCCESS;
 
         PaymentDto.Response pendingResponse =
                 paymentService.requestPaymentExternalService(reservation.getId(), paymentProviderType);
@@ -127,14 +118,44 @@ class PaymentIntegrationTest extends AbstractIntegrationTest {
 
         PaymentDto.WebhookRequest webhookRequest = PaymentDto.WebhookRequest.builder()
                 .externalPaymentId(updated.getExternalPaymentId())
-                .paymentStatus(PaymentStatus.SUCCESS)
+                .paymentStatus(성공)
                 .build();
 
         paymentService.handleWebhook(paymentProviderType, webhookRequest);
 
         //TODO paymentProviderExternalService.requestPaymentExternalApi mock으로 관리
         updated = paymentRepository.findById(pendingResponse.getPaymentId()).orElseThrow();
-        Assertions.assertEquals(PaymentStatus.SUCCESS, updated.getPaymentStatus());
+        Assertions.assertEquals(성공, updated.getPaymentStatus());
+        Assertions.assertNotNull(updated.getExternalPaymentId());
+    }
+
+    @Test
+    void 결제_실패_프로세스_통합_테스트() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        PaymentStatus 실패 = PaymentStatus.FAILED;
+
+        PaymentDto.Response pendingResponse =
+                paymentService.requestPaymentExternalService(reservation.getId(), paymentProviderType);
+
+        Assertions.assertEquals(PaymentStatus.PENDING,
+                paymentRepository.findById(pendingResponse.getPaymentId())
+                        .orElseThrow().getPaymentStatus());
+
+        latch.await(2, TimeUnit.SECONDS);
+
+        Payment updated = paymentRepository.findById(pendingResponse.getPaymentId()).orElseThrow();
+
+        PaymentDto.WebhookRequest webhookRequest = PaymentDto.WebhookRequest.builder()
+                .externalPaymentId(updated.getExternalPaymentId())
+                .paymentStatus(실패)
+                .build();
+
+        paymentService.handleWebhook(paymentProviderType, webhookRequest);
+
+        //TODO paymentProviderExternalService.requestPaymentExternalApi mock으로 관리
+        updated = paymentRepository.findById(pendingResponse.getPaymentId()).orElseThrow();
+        Assertions.assertEquals(실패, updated.getPaymentStatus());
         Assertions.assertNotNull(updated.getExternalPaymentId());
     }
 }
